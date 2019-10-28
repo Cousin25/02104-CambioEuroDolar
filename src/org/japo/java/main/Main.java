904b697fb0d7758a0676e1535d13ed7cf1188343
Andrés Cosín Zamarreño <Cousin183@gmail.com>
2019-10-28 13:06:51 +0100

    he hecho un cambio

--- src/org/japo/java/main/Main.java
+++ src/org/japo/java/main/Main.java
@@ -23,9 +23,8 @@ import java.util.Locale;
  */
 public class Main {
 
-    
     public static void main(String[] args) {
-        System.out.println("1 euro en dolares es=" +1.09925);
+        System.out.println("1 euro en dolares es=" + 1.09925);
     }
-    
+
 }
